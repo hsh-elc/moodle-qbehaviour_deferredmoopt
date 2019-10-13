2d36efe2e86de526ee4e89789642ca45b591b288
Robin Drangmeister <robin.drangmeister@stud.hs-hannover.de>
2019-10-13 21:00:30 +0200

    Implemented separate-test-feedback

--- behaviour.php
+++ behaviour.php
@@ -149,7 +149,8 @@ class qbehaviour_deferredprogrammingtask extends question_behaviour_with_save {
             return question_attempt::DISCARD;
         }
 
-        $fraction = $pendingstep->get_qt_var('score');
+        $score = $pendingstep->get_qt_var('score');
+        $fraction = $score / $this->qa->get_max_mark();
 
         $pendingstep->set_fraction($fraction);
         $pendingstep->set_state(question_state::graded_state_for_fraction($fraction));
